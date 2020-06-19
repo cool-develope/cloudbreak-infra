31aafcf8970addc4773a5f99e6fa32fd8c16abc7
cool-develope <cool199966@outlook.com>
2020-06-19 20:27:42 +0100

    Add customMessage trigger

--- lib/cognito-stack.ts
+++ lib/cognito-stack.ts
@@ -1,8 +1,10 @@
+import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
-import * as cognito from "@aws-cdk/aws-cognito";
+import * as cognito from '@aws-cdk/aws-cognito';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CognitoStack extends cdk.Stack {
-  userPool: cognito.UserPool;
+  public readonly userPool: cognito.UserPool;
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -11,19 +13,9 @@ export class CognitoStack extends cdk.Stack {
       userPoolName: 'users',
       selfSignUpEnabled: true,
       signInCaseSensitive: false,
-      userVerification: {
-        emailStyle: cognito.VerificationEmailStyle.LINK
-      },
-      userInvitation: {
-        emailSubject: 'Invite to join our awesome app!',
-        emailBody: 'Hello {username}, you have been invited to join our awesome app! Your temporary password is {####}',
-      },
       signInAliases: {
-        //Whether user is allowed to sign up or sign in with a username
         username: false,
-
-        //Whether a user is allowed to sign up or sign in with an email address
-        email: true
+        email: true,
       },
       autoVerify: { email: true },
     });
@@ -34,16 +26,41 @@ export class CognitoStack extends cdk.Stack {
      * Create a WebClient for web app
      */
     this.createWebClient();
+
+    /**
+     * Use Lambda to send custom messages
+     */
+    this.addTriggerCustomMessage();
   }
 
   createWebClient() {
     const webClient = new cognito.UserPoolClient(this, 'user-pool-web-client', {
       userPool: this.userPool,
       userPoolClientName: 'web-client',
-      // @ts-ignore
-      enabledAuthFlows: ['ALLOW_REFRESH_TOKEN_AUTH', 'ALLOW_CUSTOM_AUTH', 'ALLOW_USER_SRP_AUTH']
+      authFlows: {
+        custom: true,
+        refreshToken: true,
+        userSrp: true,
+      },
     });
 
     new cdk.CfnOutput(this, 'user-pool-web-client-id', { value: webClient.userPoolClientId });
   }
-};
+
+  addTriggerCustomMessage() {
+    const cognitoCustomMessageFunction = new lambda.Function(this, 'cognitoCustomMessage', {
+      code: lambda.Code.fromAsset(path.join(__dirname, '../', 'functions', 'cognitoCustomMessage')),
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      environment: {
+        VERIFICATION_URL: `https://my-tifo.com/account/verification`,
+        RECOVERY_URL: `https://my-tifo.com/account/recovery`,
+      },
+    });
+
+    this.userPool.addTrigger(
+      cognito.UserPoolOperation.CUSTOM_MESSAGE,
+      cognitoCustomMessageFunction,
+    );
+  }
+}
