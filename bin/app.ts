dd2953b1afa2474f15c53b39ddcf737944e9ae3e
cool-develope <cool199966@outlook.com>
2020-07-15 15:42:40 +0300

    Add common ACM

--- bin/app.ts
+++ bin/app.ts
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import { AcmStack } from '../lib/acm-stack';
 import { AppStack } from '../lib/app-stack';
 
 const app = new cdk.App();
 
+/**
+ * Register certificates in US for CloudFron
+ */
+new AcmStack(app, 'acm-us-stack', {
+  env: {
+    region: 'us-east-1',
+  },
+});
+
 new AppStack(app, 'prod', {
   prod: true,
   env: {
-    region: 'eu-central-1'
-  }
+    region: 'eu-central-1',
+  },
 });
 
 // new AppStack(app, 'dev', {
