5246130ded96cf50a89ffd2875c41ee9ea13c83d
cool-develope <cool199966@outlook.com>
2020-08-05 22:28:33 +0300

    Update table stack

---
+++ lib/table-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+
+export class TableStack extends cdk.Stack {
+  public readonly dictionaryTable: dynamodb.Table;
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    /**
+     * Dictionary
+     */
+    this.dictionaryTable = new dynamodb.Table(this, 'DictionaryTable', {
+      tableName: 'Dictionary',
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
+    });
+  }
+}
