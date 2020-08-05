3ba968ac04034ca56716dbcfea2e5431d774548f
cool-develope <cool199966@outlook.com>
2020-08-05 22:29:14 +0300

    Update ACM stack

--- lib/acm-stack.ts
+++ lib/acm-stack.ts
@@ -1,15 +1,21 @@
 import * as cdk from '@aws-cdk/core';
 import { Certificate, ValidationMethod } from '@aws-cdk/aws-certificatemanager';
 
+export interface AcmStackProps extends cdk.StackProps {
+  domainName: string;
+}
+
 export class AcmStack extends cdk.Stack {
   public readonly certificate: Certificate;
 
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: AcmStackProps) {
     super(scope, id, props);
 
+    const { domainName } = props;
+
     this.certificate = new Certificate(this, 'certificate-tifo-sport', {
-      domainName: 'tifo-sport.com',
-      subjectAlternativeNames: ['*.tifo-sport.com'],
+      domainName,
+      subjectAlternativeNames: [`*.${domainName}`],
       validationMethod: ValidationMethod.DNS,
     });
 
