cb557acb4ae36d440572d7448a909f2f7d6bd1c1
cool-develope <cool199966@outlook.com>
2020-08-08 21:14:05 +0300

    Remove recovey step

--- functions/cognito/customMessage/index.ts
+++ functions/cognito/customMessage/index.ts
@@ -23,7 +23,7 @@ const getEmailHtml = (templateFileName: string, data: any) => {
 const getPayload = (data: any) => Buffer.from(JSON.stringify(data)).toString('base64');
 
 export const handler: CognitoUserPoolTriggerHandler = (event, context, callback) => {
-  const { VERIFICATION_URL, RECOVERY_URL } = process.env;
+  const { VERIFICATION_URL } = process.env;
   const {
     codeParameter,
     userAttributes: { email_verified, name },
@@ -41,14 +41,6 @@ export const handler: CognitoUserPoolTriggerHandler = (event, context, callback)
     event.response.emailMessage = getEmailHtml('./templates/verification.html', {
       firstName: name,
       confimationUrl: url,
-    });
-
-    callback(null, event);
-  } else if (event.triggerSource === CustomMessage.ForgotPassword) {
-    const url = `${RECOVERY_URL}?data=${payload}&code=${codeParameter}`;
-    event.response.emailSubject = 'Password Recovery';
-    event.response.emailMessage = getEmailHtml('./templates/recovery.html', {
-      recoveryUrl: url,
     });
 
     callback(null, event);
