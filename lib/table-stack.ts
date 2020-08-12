9599fde695bf1a65bbce307cced946cc104ffa0f
cool-develope <cool199966@outlook.com>
2020-08-12 20:09:07 +0300

    Move table names out

--- lib/table-stack.ts
+++ lib/table-stack.ts
@@ -1,17 +1,24 @@
 import * as cdk from '@aws-cdk/core';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
+export interface TableStackProps extends cdk.StackProps {
+  dictionaryTableName: string;
+  usersTableName: string;
+}
+
 export class TableStack extends cdk.Stack {
   public readonly dictionaryTable: dynamodb.Table;
 
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: TableStackProps) {
     super(scope, id, props);
+
+    const { dictionaryTableName, usersTableName } = props;
 
     /**
      * Dictionary
      */
     this.dictionaryTable = new dynamodb.Table(this, 'DictionaryTable', {
-      tableName: 'Dictionary',
+      tableName: dictionaryTableName,
       partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
     });
@@ -20,7 +27,7 @@ export class TableStack extends cdk.Stack {
      * Users
      */
     this.dictionaryTable = new dynamodb.Table(this, 'UsersTable', {
-      tableName: 'Users',
+      tableName: usersTableName,
       partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
     });
