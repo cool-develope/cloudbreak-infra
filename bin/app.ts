3c6f516ea14f12af6925b1fa989d06b5dc83c817
cool-develope <cool199966@outlook.com>
2020-08-12 21:28:17 +0300

    Added env and all stacks

--- bin/app.ts
+++ bin/app.ts
@@ -2,25 +2,78 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { AcmStack } from '../lib/acm-stack';
+import { CognitoStack } from '../lib/cognito-stack';
+import { ApiDomainStack } from '../lib/api-domain-stack';
+import { ApiStack } from '../lib/api-stack';
+import { WebSiteStack } from '../lib/website-stack';
 import { TableStack } from '../lib/table-stack';
 import { EventsStack } from '../lib/events-stack';
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: `.env.${process.env.TIFO_ENV || 'dev'}`,
+});
 
 const app = new cdk.App();
 
 /**
- * Register certificates in US for CloudFron
+ * Register certificates in US for CloudFront
  */
 new AcmStack(app, 'acm-us-stack', {
+  domainName: process.env.ZONE_NAME || '',
   env: {
     region: 'us-east-1',
   },
 });
 
 /**
- * Create DynamoDB tables
+ * Create Cognito
  */
-new TableStack(app, 'table-stack');
+new CognitoStack(app, 'cognito-stack', {
+  signinUrl: process.env.SIGNIN_URL || '',
 });
 
+/**
+ * Create DynamoDB tables
+ */
+new TableStack(app, 'table-stack', {
+  dictionaryTableName: 'Dictionary',
+  usersTableName: 'Users',
+});
 
+/**
+ * Create GraphQL API
+ */
+new ApiStack(app, 'api-stack', {
+  userPoolId: process.env.USER_POOL_ID || '',
+  dictionaryTableName: 'Dictionary',
+  usersTableName: 'Users',
+});
+
+/**
+ * Create S3 Bucket for admin dashboard
+ */
+new WebSiteStack(app, 'admin-website-stack', {
+  bucketName: process.env.ADMIN_BUCKET_NAME || '',
+  bucketRefererHeader: '9c33fyuWNcB8bn9r24',
+  zoneId: process.env.ZONE_ID || '',
+  zoneName: process.env.ZONE_NAME || '',
+  domain: `admin.${process.env.ZONE_NAME}`,
+  certificateArn: process.env.US_CERTIFICATE_ARN || '',
+});
+
+/**
+ * Create custom domain for AppSync
+ */
+new ApiDomainStack(app, 'api-domain-stack', {
+  zoneId: process.env.ZONE_ID || '',
+  zoneName: process.env.ZONE_NAME || '',
+  apiDomain: `api.${process.env.ZONE_NAME}`,
+  appSyncDomain: process.env.APPSYNC_DOMAIN || '',
+  certificateArn: process.env.US_CERTIFICATE_ARN || '',
+});
+
+/**
+ * EventBridge
+ */
 new EventsStack(app, 'events-stack', {});
