55f84cff8c5dffe09a1b84e1ee467792cf777993
cool-develope <cool199966@outlook.com>
2020-08-17 19:30:29 +0300

    Update storage stack

--- lib/storage-stack.ts
+++ lib/storage-stack.ts
@@ -1,28 +1,32 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
-// import * as cloudfront from '@aws-cdk/aws-cloudfront';
-// import * as route53 from '@aws-cdk/aws-route53';
-// import * as route53tg from '@aws-cdk/aws-route53-targets';
-// import { Certificate, ICertificate } from '@aws-cdk/aws-certificatemanager';
-import { CloudFrontWebDistribution, OriginProtocolPolicy } from '@aws-cdk/aws-cloudfront';
-import { SIGUSR2 } from 'constants';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as route53tg from '@aws-cdk/aws-route53-targets';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { Certificate, ICertificate } from '@aws-cdk/aws-certificatemanager';
+import {
+  CloudFrontWebDistribution,
+  OriginAccessIdentity,
+  LambdaEdgeEventType,
+} from '@aws-cdk/aws-cloudfront';
 
 export interface StorageStackProps extends cdk.StackProps {
   bucketName: string;
-  // bucketRefererHeader?: string;
-  // zoneId: string;
-  // zoneName: string;
-  // domain: string;
-  // certificateArn: string;
+  zoneId: string;
+  zoneName: string;
+  domain: string;
+  certificateArn: string;
+  imagesOriginRequestArn: string;
 }
 
 export class StorageStack extends cdk.Stack {
-  // public readonly zone: route53.IHostedZone;
+  public readonly zone: route53.IHostedZone;
 
   constructor(scope: cdk.Construct, id: string, props: StorageStackProps) {
     super(scope, id, props);
 
-    const { bucketName } = props;
+    const { bucketName, zoneId, zoneName, domain, certificateArn, imagesOriginRequestArn } = props;
 
     /**
      * Create S3 Bucket. Enable static Web Site
@@ -32,18 +36,20 @@ export class StorageStack extends cdk.Stack {
     /**
      * Create CloudFront
      */
-    // const certificate = Certificate.fromCertificateArn(this, 'us-certificate', certificateArn);
-    // const distribution = this.createCloudFrontDistribution(
-    //   bucket.bucketWebsiteDomainName,
-    //   domain,
-    //   certificate,
-    //   bucketRefererHeader,
-    // );
+    const certificate = Certificate.fromCertificateArn(this, 'us-certificate', certificateArn);
+    const imagesOriginRequestVersion = lambda.Version.fromVersionArn(this, 'imagesOriginRequestVersion', imagesOriginRequestArn);
+    
+    const distribution = this.createCloudFrontDistribution(
+      bucket,
+      domain,
+      certificate,
+      imagesOriginRequestVersion,
+    );
 
     /**
      * Add record to Route53
      */
-    // this.createDomainRecord(zoneId, zoneName, domain, distribution);
+    this.createDomainRecord(zoneId, zoneName, domain, distribution);
   }
 
   createS3Bucket(bucketName: string) {
@@ -72,65 +78,62 @@ export class StorageStack extends cdk.Stack {
       ],
     });
 
-    // const bucketPolicy = bucket.grantPublicAccess('*', 's3:GetObject');
-    // bucketPolicy.resourceStatement!.addResources(bucket.bucketArn);
-    // bucketPolicy.resourceStatement!.sid = 'AllowGetObject';
-    // if (bucketRefererHeader) {
-    //   bucketPolicy.resourceStatement!.addCondition('StringEquals', {
-    //     'aws:Referer': bucketRefererHeader,
-    //   });
-    // }
-
     return bucket;
   }
 
-  // createCloudFrontDistribution(
-  //   bucketWebsiteDomain: string,
-  //   domain: string,
-  //   certificate: ICertificate,
-  //   bucketRefererHeader?: string,
-  // ) {
-  //   let originHeaders;
-  //   if (bucketRefererHeader) {
-  //     originHeaders = {
-  //       Referer: bucketRefererHeader,
-  //     };
-  //   }
+  createCloudFrontDistribution(
+    bucket: s3.Bucket,
+    domain: string,
+    certificate: ICertificate,
+    imagesOriginRequestVersion: lambda.IVersion,
+  ) {
+    const cloudFrontOAI = new OriginAccessIdentity(this, `${domain}-OAI`, {
+      comment: `OAI for ${bucket.bucketName}`,
+    });
 
-  //   const distribution = new cloudfront.CloudFrontWebDistribution(this, 'cloudfront-website', {
-  //     originConfigs: [
-  //       {
-  //         customOriginSource: {
-  //           domainName: bucketWebsiteDomain,
-  //           originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
-  //         },
-  //         behaviors: [{ isDefaultBehavior: true, compress: true }],
-  //         originHeaders,
-  //       },
-  //     ],
-  //     viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(certificate, {
-  //       aliases: [domain],
-  //     }),
-  //   });
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, `${domain}-cloudfront`, {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: cloudFrontOAI,
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+              lambdaFunctionAssociations: [
+                {
+                  eventType: LambdaEdgeEventType.ORIGIN_REQUEST,
+                  lambdaFunction: imagesOriginRequestVersion
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(certificate, {
+        aliases: [domain],
+      }),
+    });
 
-  //   return distribution;
-  // }
+    return distribution;
+  }
 
-  // createDomainRecord(
-  //   zoneId: string,
-  //   zoneName: string,
-  //   domain: string,
-  //   distribution: CloudFrontWebDistribution,
-  // ) {
-  //   const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'zone-tifo-sport', {
-  //     hostedZoneId: zoneId,
-  //     zoneName,
-  //   });
+  createDomainRecord(
+    zoneId: string,
+    zoneName: string,
+    domain: string,
+    distribution: CloudFrontWebDistribution,
+  ) {
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'zone-tifo-sport', {
+      hostedZoneId: zoneId,
+      zoneName,
+    });
 
-  //   new route53.ARecord(this, `record-${domain}`, {
-  //     zone: hostedZone,
-  //     recordName: domain,
-  //     target: route53.RecordTarget.fromAlias(new route53tg.CloudFrontTarget(distribution)),
-  //   });
-  // }
+    new route53.ARecord(this, `record-${domain}`, {
+      zone: hostedZone,
+      recordName: domain,
+      target: route53.RecordTarget.fromAlias(new route53tg.CloudFrontTarget(distribution)),
+    });
+  }
 }
