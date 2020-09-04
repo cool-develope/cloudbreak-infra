412f7f0680e34b7418f4ccbac1257774a27140f0
cool-develope <cool199966@outlook.com>
2020-09-04 10:51:00 +0300

    Added function - federationsPrivate

---
+++ functions/api/federationsPrivate/index.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+import * as AWS from 'aws-sdk';
+import { Handler } from 'aws-lambda';
+
+const { MAIN_TABLE_NAME, IMAGES_DOMAIN, ES_DOMAIN } = process.env;
+
+export const handler: Handler = async (event) => {
+  const {
+    arguments: { filter, limit, from },
+    identity: { sub },
+    info: { fieldName },
+  } = event;
+
+  return {
+    items: [
+      { id: '10-00', name: 'Federation A' },
+      { id: '10-01', name: 'Federation B' },
+    ],
+  };
+};
