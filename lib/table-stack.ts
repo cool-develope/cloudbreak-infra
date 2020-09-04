fd3205ebfc2f84c247c1bdeaf3e1ad1e6904c9be
cool-develope <cool199966@outlook.com>
2020-09-04 08:26:04 +0300

    Added DynamoDB stream handler

--- lib/table-stack.ts
+++ lib/table-stack.ts
@@ -1,5 +1,11 @@
+import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { DynamoEventSource } from '@aws-cdk/aws-lambda-event-sources';
+import { RetentionDays } from '@aws-cdk/aws-logs';
+import { PolicyStatement, Effect } from '@aws-cdk/aws-iam';
+import { Duration } from '@aws-cdk/core';
 
 export interface TableStackProps extends cdk.StackProps {
   esDomain: string;
@@ -30,6 +36,59 @@ export class TableStack extends cdk.Stack {
       partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
       stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
+    });
+
+    const dynamoStreamMainFunction = this.getFunction(
+      'events-dynamoStreamMain',
+      'events-dynamoStreamMain',
+      'dynamoStreamMain',
+      {
+        MAIN_TABLE_NAME: mainTableName,
+        ES_DOMAIN: esDomain,
+      },
+    );
+
+    dynamoStreamMainFunction.addEventSource(
+      new DynamoEventSource(mainTable, {
+        batchSize: 100,
+        maxBatchingWindow: Duration.seconds(2),
+        startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+      }),
+    );
+
+    const mainPolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+    });
+
+    mainPolicy.addActions(
+      'es:ESHttpDelete',
+      'es:ESHttpGet',
+      'es:ESHttpHead',
+      'es:ESHttpPost',
+      'es:ESHttpPut',
+      'es:ESHttpPatch',
+    );
+    mainPolicy.addResources('*');
+
+    // Allow Lambda access VPC
+    // ec2:CreateNetworkInterface
+    // ec2:DescribeNetworkInterfaces
+    // ec2:DeleteNetworkInterface
+
+    dynamoStreamMainFunction.addToRolePolicy(mainPolicy);
+  }
+
+  getFunction(id: string, functionName: string, folderName: string, environment?: any) {
+    return new lambda.Function(this, id, {
+      functionName,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../', 'functions', 'events', folderName)),
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      environment,
+      logRetention: RetentionDays.THREE_DAYS,
+      tracing: lambda.Tracing.ACTIVE,
+      timeout: cdk.Duration.seconds(120),
+      memorySize: 1024,
     });
   }
 }
