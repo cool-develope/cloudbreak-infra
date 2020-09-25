f993b3b7ebd86fc292f0291f9827082d82e77483
cool-develope <cool199966@outlook.com>
2020-09-25 11:56:22 +0300

    Added PrivateStorageStack

---
+++ lib/private-storage-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+
+export interface PrivateStorageStackProps extends cdk.StackProps {
+  bucketName: string;
+}
+
+export class PrivateStorageStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: PrivateStorageStackProps) {
+    super(scope, id, props);
+
+    const { bucketName } = props;
+
+    const bucket = new s3.Bucket(this, `s3-bucket-${bucketName}`, {
+      bucketName,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.KMS_MANAGED,
+    });
+  }
+}
