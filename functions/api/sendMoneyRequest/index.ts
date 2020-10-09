47bc48c4363ad6af0ac3971687ae87a3869a6690
cool-develope <cool199966@outlook.com>
2020-10-09 23:36:44 +0300

    Added rejectMoneyRequest and moneyRequests

--- functions/api/sendMoneyRequest/index.ts
+++ functions/api/sendMoneyRequest/index.ts
@@ -2,6 +2,42 @@
 import * as AWS from 'aws-sdk';
 import * as OneSignal from 'onesignal-node';
 import { Handler } from 'aws-lambda';
+// @ts-ignore
+import { v4 as uuidv4 } from 'uuid';
+import DynamoHelper from './dynamoHelper';
+import { stat } from 'fs/promises';
+
+enum FieldName {
+  sendMoneyRequest = 'sendMoneyRequest',
+  rejectMoneyRequest = 'rejectMoneyRequest',
+  moneyRequests = 'moneyRequests',
+}
+
+enum MoneyRequestStatus {
+  Pending = 'Pending',
+  Paid = 'Paid',
+  Rejected = 'Rejected',
+}
+
+interface MoneyRequest {
+  id: string;
+  amount: number;
+  note: string;
+  status: string;
+  createDate: string;
+  user: UserPublic;
+  fromMe: boolean;
+}
+
+interface Image {
+  url: string;
+}
+
+interface UserPublic {
+  firstName?: string;
+  lastName?: string;
+  photo?: Image;
+}
 
 const {
   MAIN_TABLE_NAME = '',
@@ -12,6 +48,25 @@ const {
 } = process.env;
 
 const db = new AWS.DynamoDB.DocumentClient();
+const eventbridge = new AWS.EventBridge();
+const dynamoHelper = new DynamoHelper(db, MAIN_TABLE_NAME);
+
+const putEvents = (type: string, detail: any) => {
+  const params = {
+    Entries: [
+      {
+        Source: 'tifo.api',
+        EventBusName: 'default',
+        Time: new Date(),
+        DetailType: type,
+        Detail: JSON.stringify(detail),
+      },
+    ],
+  };
+
+  console.log(type, detail);
+  return eventbridge.putEvents(params).promise();
+};
 
 const getDeviceIds = async (pk: string) => {
   const getResult = await getItem(pk, 'devices');
@@ -20,6 +75,25 @@ const getDeviceIds = async (pk: string) => {
   }
 
   return [];
+};
+
+const getAllMoneyRequests = (userId: string, status: MoneyRequestStatus) => {
+  const sk = 'metadata';
+  const pk = 'money-request#';
+
+  const params = {
+    TableName: MAIN_TABLE_NAME,
+    IndexName: 'GSI1',
+    KeyConditionExpression: 'sk = :sk and begins_with(pk, :pk)',
+    FilterExpression: 'senderUserId = :userId or recipientUserId = :userId',
+    ExpressionAttributeValues: {
+      ':sk': sk,
+      ':pk': pk,
+      ':userId': userId,
+    },
+  };
+
+  return db.query(params).promise();
 };
 
 const sendPushNotifications = (player_ids: string[] = [], data: any) => {
@@ -64,13 +138,22 @@ const getItem = (pk: string, sk: string) => {
 
 const getImageUrl = (photo: string = '') => (photo ? `https://${IMAGES_DOMAIN}/${photo}` : '');
 
-const sendMoneyRequest = async (
-  senderUser: any,
-  receiverUser: any,
-  email: string,
-  amount: string,
-  note: string,
-): Promise<string[]> => {
+const getTypeUser = ({ firstName, lastName, photo }: any): UserPublic => ({
+  firstName,
+  lastName,
+  photo: getTypeImage(photo),
+});
+
+const getTypeImage = (photo: string = '') => ({
+  url: photo ? `https://${IMAGES_DOMAIN}/${photo}` : '',
+});
+
+const sendMoneyRequest = async (sub: string, input: any): Promise<string[]> => {
+  const { email, amount, note } = input;
+
+  const { Item: senderUser } = await getItem(`user#${sub}`, 'metadata');
+  const { Items: recipients } = await scanItems('user#', 'metadata', email);
+  const [recipientUser] = recipients;
   const errors: string[] = [];
 
   /**
@@ -78,12 +161,14 @@ const sendMoneyRequest = async (
    * TODO: check receiver devices ids
    */
 
-  if (!receiverUser) {
+  if (!recipientUser) {
     errors.push('Recipient Not Found');
     return errors;
   }
 
-  const ids = await getDeviceIds(receiverUser.pk);
+  const ids = await getDeviceIds(recipientUser.pk);
+  const senderUserId = sub;
+  const recipientUserId = recipientUser.pk.replace('user#', '');
 
   try {
     const data = {
@@ -106,22 +191,83 @@ const sendMoneyRequest = async (
     console.log(e);
   }
 
+  const requestPk = `money-request#${uuidv4()}`;
+  const requestSk = 'metadata';
+
+  await dynamoHelper.updateItem(requestPk, requestSk, {
+    amount,
+    note,
+    status: MoneyRequestStatus.Pending,
+    createdAt: new Date().toISOString(),
+    senderUserId,
+    recipientUserId,
+  });
+
+  await putEvents('SendMoneyRequest', {
+    senderSub: senderUser.pk.replace('user#', ''),
+    senderEmail: senderUser.email,
+    senderFirstName: senderUser.firstName,
+    senderLastName: senderUser.lastName,
+    senderPhoto: getImageUrl(senderUser.photo),
+    recipientSub: recipientUserId,
+    recipientEmail: email,
+    amount,
+    note,
+  });
+
   return errors;
 };
 
-export const handler: Handler = async (event): Promise<{ errors: string[] }> => {
+const rejectMoneyRequest = async (sub: string, input: any): Promise<string[]> => {
+  const { requestId } = input;
+  return [];
+};
+
+const moneyRequests = async (sub: string, filter: any): Promise<MoneyRequest[]> => {
+  const { status } = filter;
+  const { Items } = await getAllMoneyRequests(sub, status);
+  const resultItems: MoneyRequest[] = [];
+
+  for (const item of Items) {
+    // senderUserId	recipientUserId
+    // const { Item } = await dynamoHelper.getItem(`user#${}`, 'metadata')
+    const user = {};
+
+    resultItems.push({
+      id: item.pk.replace('money-request#', ''),
+      amount: item.amount,
+      note: item.note,
+      status: item.status,
+      createDate: item.createAt,
+      user,
+      fromMe: item.senderUserId === sub,
+    });
+  }
+
+  return resultItems;
+};
+
+export const handler: Handler = async (event): Promise<{ errors?: string[]; items?: any[] }> => {
   const {
-    arguments: {
-      input: { email, amount, note },
-    },
+    arguments: { input, filter },
     identity: { sub },
     info: { fieldName },
   } = event;
 
-  const { Item: senderUser } = await getItem(`user#${sub}`, 'metadata');
-  const { Items: receivers } = await scanItems('user#', 'metadata', email);
-  const [receiverUser] = receivers;
+  const field = fieldName as FieldName;
 
-  const errors = await sendMoneyRequest(senderUser, receiverUser, email, amount, note);
-  return { errors };
+  if (field === FieldName.sendMoneyRequest) {
+    const errors = await sendMoneyRequest(sub, input);
+    return { errors };
+  }
+  if (field === FieldName.rejectMoneyRequest) {
+    const errors = await rejectMoneyRequest(sub, input);
+    return { errors };
+  }
+  if (field === FieldName.moneyRequests) {
+    const items = await moneyRequests(sub, filter);
+    return { items };
+  }
+
+  throw Error('Query not supported');
 };
