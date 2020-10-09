652cd3444ee248360ec88ce8be9e1d8b4175d07d
cool-develope <cool199966@outlook.com>
2020-10-09 23:41:13 +0300

    Use wildcard search

--- functions/api/feed/index.ts
+++ functions/api/feed/index.ts
@@ -73,8 +73,10 @@ const getQueryBySearch = (search: string) =>
         bool: {
           should: [
             {
-              match: {
-                title: search,
+              wildcard: {
+                title: {
+                  value: `*${search}*`,
+                },
               },
             },
             {
