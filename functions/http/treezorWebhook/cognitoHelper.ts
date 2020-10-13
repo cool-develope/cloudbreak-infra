46170ce1784f435e25de6e9fee2ade3694cd9a8c
cool-develope <cool199966@outlook.com>
2020-10-13 13:09:03 +0300

    Added groups to helper

--- functions/http/treezorWebhook/cognitoHelper.ts
+++ functions/http/treezorWebhook/cognitoHelper.ts
@@ -1,9 +1,18 @@
-enum TrzAttributes {
+enum CognitoAttributes {
   trzChildren = 'custom:trzChildren',
   trzUserId = 'custom:trzUserId',
   trzScopes = 'custom:trzScopes',
   trzWalletsId = 'custom:trzWalletsId',
   trzCardsId = 'custom:trzCardsId',
+  clubs = 'custom:clubs',
+  teams = 'custom:teams',
+  federations = 'custom:federations',
+}
+
+export enum CognitoGroup {
+  ClubCoaches = 'club-coaches',
+  ClubOwners = 'club-owners',
+  FederationOwners = 'federation-owners',
 }
 
 enum TrzAttributesDefault {
@@ -48,6 +57,24 @@ class CognitoHelper {
     this.userPoolId = userPoolId;
   }
 
+  addUserToGroup(group: CognitoGroup) {
+    const params = {
+      GroupName: group,
+      UserPoolId: this.userPoolId,
+      Username: this.sub,
+    };
+    return this.cognito.adminAddUserToGroup(params).promise();
+  }
+
+  removeUserFromGroup(group: CognitoGroup) {
+    const params = {
+      GroupName: group,
+      UserPoolId: this.userPoolId,
+      Username: this.sub,
+    };
+    return this.cognito.adminRemoveUserFromGroup(params).promise();
+  }
+
   async getUserAttributes() {
     const params = {
       UserPoolId: this.userPoolId,
@@ -64,12 +91,13 @@ class CognitoHelper {
       Username: this.sub,
     };
 
+    console.log('Added new attributes:', params);
     return this.cognito.adminUpdateUserAttributes(params).promise();
   }
 
   async addChildrenData(userId: number, cards?: number[], wallets?: number[]) {
     const attr = await this.getUserAttributes();
-    const trzChildrenAttr = attr.find((a) => a.Name === TrzAttributes.trzChildren);
+    const trzChildrenAttr = attr.find((a) => a.Name === CognitoAttributes.trzChildren);
     let trzChildren: TrzChild[] = [];
     try {
       if (trzChildrenAttr && trzChildrenAttr.Value !== TrzAttributesDefault.trzChildren) {
@@ -82,10 +110,67 @@ class CognitoHelper {
     const newTrzChildrenObj = this.addPropsToObject(trzChildren, userId, cards, wallets);
     const newTrzChildrenObjStr = JSON.stringify(newTrzChildrenObj);
     const newTrzChildrenAttr: NameValue = {
-      Name: TrzAttributes.trzChildren,
+      Name: CognitoAttributes.trzChildren,
       Value: newTrzChildrenObjStr,
     };
     await this.updateUserAttributes([newTrzChildrenAttr]);
+  }
+
+  stringToSet(str?: string) {
+    const a = str ? String(str).split(', ') : [];
+    return new Set(a);
+  }
+
+  setToString(set: Set<string>) {
+    return [...set.values()].join(', ');
+  }
+
+  addClub(clubId: string) {
+    return this.addOrRemoveValue(CognitoAttributes.clubs, clubId, false);
+  }
+
+  addTeam(teamId: string) {
+    return this.addOrRemoveValue(CognitoAttributes.teams, teamId, false);
+  }
+
+  addFederation(federationId: string) {
+    return this.addOrRemoveValue(CognitoAttributes.federations, federationId, false);
+  }
+
+  removeClub(clubId: string) {
+    return this.addOrRemoveValue(CognitoAttributes.clubs, clubId, true);
+  }
+
+  removeTeam(teamId: string) {
+    return this.addOrRemoveValue(CognitoAttributes.teams, teamId, true);
+  }
+
+  removeFederation(federationId: string) {
+    return this.addOrRemoveValue(CognitoAttributes.federations, federationId, true);
+  }
+
+  private async addOrRemoveValue(
+    attributeName: CognitoAttributes,
+    value: string,
+    isRemove: boolean,
+  ) {
+    const attributes = await this.getUserAttributes();
+    const clubsAttr = attributes.find((a) => a.Name === attributeName);
+    const values = this.stringToSet(clubsAttr?.Value);
+
+    if (isRemove) {
+      values.delete(value);
+    } else {
+      values.add(value);
+    }
+
+    const newValue = this.setToString(values);
+    const newAttr: NameValue = {
+      Name: attributeName,
+      Value: newValue,
+    };
+
+    await this.updateUserAttributes([newAttr]);
   }
 
   private addPropsToObject(
