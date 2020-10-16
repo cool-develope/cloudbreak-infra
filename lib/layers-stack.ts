69e08f8ea2831083ad99f1c99e45ee46e55eed1f
cool-develope <cool199966@outlook.com>
2020-10-16 10:27:41 +0300

    Remove layers-common-code

--- lib/layers-stack.ts
+++ lib/layers-stack.ts
@@ -13,14 +13,6 @@ export class LayersStack extends cdk.Stack {
       layerVersionName: 'common-modules',
     });
 
-    const commonCode = new lambda.LayerVersion(this, 'layers-common-code', {
-      code: lambda.Code.fromAsset(path.join(__dirname, '../', 'layers', 'common-code')),
-      compatibleRuntimes: [lambda.Runtime.NODEJS_12_X],
-      description: 'Models, DynamoHelper',
-      layerVersionName: 'common-code',
-    });
-
     new cdk.CfnOutput(this, 'layer-modules', { value: commonModules.layerVersionArn });
-    new cdk.CfnOutput(this, 'layer-code', { value: commonCode.layerVersionArn });
   }
 }
