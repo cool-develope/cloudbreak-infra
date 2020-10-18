75728eff32233f8c42e16bd5b1097a742ade586c
cool-develope <cool199966@outlook.com>
2020-10-19 01:32:57 +0300

    Added access checks

--- functions/cognito/defineAuthChallenge/index.ts
+++ functions/cognito/defineAuthChallenge/index.ts
@@ -1,7 +1,93 @@
+// @ts-ignore
+import * as AWS from 'aws-sdk';
 import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
 
+export enum CognitoGroup {
+  ClubCoaches = 'club-coaches',
+  ClubOwners = 'club-owners',
+  FederationOwners = 'federation-owners',
+  TifoManager = 'tifo-manager',
+}
+
+interface ListGroupsForUser {
+  Groups: { GroupName: string }[] | null;
+}
+
+const {
+  COGNITO_WEB_CLIENT_ID = '',
+  COGNITO_MOBILE_CLIENT_ID = '',
+  COGNITO_MANAGER_CLIENT_ID = '',
+} = process.env;
+
+const cognito = new AWS.CognitoIdentityServiceProvider();
+
+const getGroups = async (userPoolId: string, sub: string): Promise<string[]> => {
+  const params = {
+    UserPoolId: userPoolId,
+    Username: sub,
+  };
+
+  const data: ListGroupsForUser = await cognito.adminListGroupsForUser(params).promise();
+
+  if (data && data.Groups && data.Groups.length) {
+    return data.Groups.map(({ GroupName }) => GroupName);
+  }
+
+  return [];
+};
+
+const canLoginToWeb = async (userPoolId: string, sub: string) => {
+  const groups = await getGroups(userPoolId, sub);
+  const allowed =
+    groups.includes(CognitoGroup.ClubCoaches) ||
+    groups.includes(CognitoGroup.ClubOwners) ||
+    groups.includes(CognitoGroup.FederationOwners) ||
+    groups.includes(CognitoGroup.TifoManager);
+  // console.log('canLoginToWeb', groups, allowed);
+
+  return allowed;
+};
+
+const canLoginToManage = async (userPoolId: string, sub: string) => {
+  const groups = await getGroups(userPoolId, sub);
+  const allowed = groups.includes(CognitoGroup.TifoManager);
+  // console.log('canLoginToManage', groups, allowed);
+  return allowed;
+};
+
+const accessAllowed = async (clientId: string, userPoolId: string, sub: string) => {
+  if (clientId === COGNITO_MOBILE_CLIENT_ID) {
+    return true;
+  } else if (clientId === COGNITO_WEB_CLIENT_ID) {
+    return await canLoginToWeb(userPoolId, sub);
+  } else if (clientId === COGNITO_MANAGER_CLIENT_ID) {
+    return await canLoginToManage(userPoolId, sub);
+  }
+
+  return false;
+};
+
 export const handler: CognitoUserPoolTriggerHandler = async (event) => {
-  if (
+  console.log(JSON.stringify(event, null, 4));
+
+  const {
+    userPoolId,
+    userName = '',
+    callerContext: { clientId },
+  } = event;
+
+  const allowed = await accessAllowed(clientId, userPoolId, userName);
+
+  if (!allowed) {
+    console.log('Access Denied', {
+      clientId,
+      userPoolId,
+      userName,
+    });
+
+    event.response.issueTokens = false;
+    event.response.failAuthentication = true;
+  } else if (
     event.request.session &&
     event.request.session.find((attempt) => attempt.challengeName !== 'CUSTOM_CHALLENGE')
   ) {
