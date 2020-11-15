c384a91d6de7eba8c93fecbb52da4f273a15b9b2
cool-develope <cool199966@outlook.com>
2020-11-15 22:40:59 +0200

    Added push notifications

--- functions/events/notifications/index.ts
+++ functions/events/notifications/index.ts
@@ -5,15 +5,37 @@ import { EventBridgeHandler } from 'aws-lambda';
 import { v4 as uuidv4 } from 'uuid';
 // @ts-ignore
 import { Client } from '@elastic/elasticsearch';
-import { NotificationType, KeyValue } from './common-code/nodejs/types/notifications';
+import {
+  NotificationType,
+  KeyValue,
+  NotificationTeamInvitation,
+  NotificationKycReview,
+  NotificationInviteParent,
+  NotificationChildInvitation,
+  NotificationSendMoneyRequest,
+} from './common-code/nodejs/types/notifications';
 import { NotificationsModel } from './common-code/nodejs/models';
 import DynamoHelper from './common-code/nodejs/dynamoHelper';
+import PushNotifications from './pushNotifications';
 
 const { MAIN_TABLE_NAME = '', IMAGES_DOMAIN = '', ES_DOMAIN } = process.env;
 const db = new AWS.DynamoDB.DocumentClient();
 const es = new Client({ node: ES_DOMAIN });
 const dynamoHelper = new DynamoHelper(db, MAIN_TABLE_NAME);
 const notificationsModel = new NotificationsModel(db, MAIN_TABLE_NAME, IMAGES_DOMAIN, uuidv4, es);
+const pushNotifications = new PushNotifications(IMAGES_DOMAIN);
+
+const getDeviceIds = async (userId: string) => {
+  const res = await dynamoHelper.getItem(`user#${userId}`, 'devices');
+  return res?.Item?.ids?.values || [];
+};
+
+const getImageUrl = (image: string = '') => (image ? `https://${IMAGES_DOMAIN}/${image}` : '');
+
+const getUserLanguage = async (userId: string) => {
+  const { Item: userData } = await dynamoHelper.getItem(`user#${userId}`, 'metadata');
+  return userData?.language || 'en';
+};
 
 const objToKeyValueArray = (obj: any): KeyValue[] =>
   Object.keys(obj).map((key) => ({
@@ -63,125 +85,148 @@ const acceptedPaidEventHandler = async (type: NotificationType, detail: any) => 
   }
 };
 
+const sendTeamInvitation = async (type: NotificationType, detail: NotificationTeamInvitation) => {
+  const deviceIds = await getDeviceIds(detail.sub);
+  const language = await getUserLanguage(detail.sub);
+  await pushNotifications.send(language, deviceIds, type, detail);
+  await notificationsModel.create(detail.sub, {
+    type,
+    attributes: objToKeyValueArray({
+      teamId: detail.teamId,
+      teamName: detail.teamName,
+      role: detail.role,
+    }),
+  });
+};
+
+const sendKycReview = async (type: NotificationType, detail: NotificationKycReview) => {
+  const sub = detail.sub;
+  const deviceIds = await getDeviceIds(sub);
+  const language = await getUserLanguage(sub);
+  await pushNotifications.send(language, deviceIds, type, detail);
+  await notificationsModel.create(detail.sub, {
+    type,
+    attributes: objToKeyValueArray({
+      status: detail.status,
+    }),
+  });
+};
+
+const sendInviteParent = async (type: NotificationType, detail: NotificationInviteParent) => {
+  const sub = detail.childParentSub;
+  const deviceIds = await getDeviceIds(sub);
+  const language = await getUserLanguage(sub);
+  await pushNotifications.send(language, deviceIds, type, detail);
+  await notificationsModel.create(detail.childParentSub, {
+    type,
+    attributes: objToKeyValueArray({
+      childUserId: detail.childSub,
+      childEmail: detail.childEmail,
+      childFirstName: detail.childFirstName,
+      childLastName: detail.childLastName,
+      childBirthDate: detail.childBirthDate,
+      childPhoto: detail.childPhoto,
+    }),
+  });
+};
+
+const sendChildInvitation = async (type: NotificationType, detail: NotificationChildInvitation) => {
+  const sub = detail.childSub;
+  const deviceIds = await getDeviceIds(sub);
+  const language = await getUserLanguage(sub);
+  await pushNotifications.send(language, deviceIds, type, detail);
+  await notificationsModel.create(detail.childSub, {
+    type,
+    attributes: objToKeyValueArray({
+      parentUserId: detail.parentSub,
+      parentFirstName: detail.parentFirstName,
+      parentLastName: detail.parentLastName,
+    }),
+  });
+};
+
+const sendSendMoneyRequest = async (
+  type: NotificationType,
+  detail: NotificationSendMoneyRequest,
+) => {
+  const sub = detail.recipientSub;
+  const deviceIds = await getDeviceIds(sub);
+  const language = await getUserLanguage(sub);
+  await pushNotifications.send(language, deviceIds, type, detail);
+
+  const parent = await getParentUser(detail.senderSub);
+  if (parent) {
+    /**
+     * Add notification for parent
+     */
+    await notificationsModel.create(parent.sub, {
+      type: NotificationType.ChildSendMoneyRequest,
+      attributes: objToKeyValueArray({
+        childFirstName: detail.senderFirstName,
+        childLastName: detail.senderLastName,
+        recipientFirstName: detail.recipientFirstName,
+        recipientLastName: detail.recipientLastName,
+        amount: detail.amount,
+        note: detail.note,
+      }),
+    });
+  }
+
+  await notificationsModel.create(detail.recipientSub, {
+    type,
+    attributes: objToKeyValueArray({
+      senderUserId: detail.senderSub,
+      senderEmail: detail.senderEmail,
+      senderFirstName: detail.senderFirstName,
+      senderLastName: detail.senderLastName,
+      senderPhoto: detail.senderPhoto,
+      amount: detail.amount,
+      note: detail.note,
+    }),
+  });
+};
+
 export const handler: EventBridgeHandler<any, any, any> = async (event) => {
   const { detail } = event;
-  const type = event['detail-type'];
+  const type: NotificationType = event['detail-type'];
   console.log(type, detail);
 
   switch (type) {
     case NotificationType.SendTeamInvitation:
-      await notificationsModel.create(detail.sub, {
-        type,
-        attributes: objToKeyValueArray({
-          teamId: detail.teamId,
-          teamName: detail.teamName,
-          role: detail.role,
-        }),
-      });
+      await sendTeamInvitation(type, detail);
       break;
 
     case NotificationType.DeclineTeamInvitation:
-      await notificationsModel.create(detail.sub, {
-        type,
-        attributes: objToKeyValueArray({
-          teamId: detail.teamId,
-          teamName: detail.teamName,
-          role: detail.role,
-        }),
-      });
+      await sendTeamInvitation(type, detail);
       break;
 
     case NotificationType.AcceptTeamInvitation:
-      await notificationsModel.create(detail.sub, {
-        type,
-        attributes: objToKeyValueArray({
-          teamId: detail.teamId,
-          teamName: detail.teamName,
-          role: detail.role,
-        }),
-      });
+      await sendTeamInvitation(type, detail);
       break;
 
     case NotificationType.SendMoneyRequest:
-      const parent = await getParentUser(detail.senderSub);
-      if (parent) {
-        /**
-         * Add notification for parent
-         */
-        await notificationsModel.create(parent.sub, {
-          type: NotificationType.ChildSendMoneyRequest,
-          attributes: objToKeyValueArray({
-            childFirstName: detail.senderFirstName,
-            childLastName: detail.senderLastName,
-            recipientFirstName: detail.recipientFirstName,
-            recipientLastName: detail.recipientLastName,
-            amount: detail.amount,
-            note: detail.note,
-          }),
-        });
-      }
-
-      await notificationsModel.create(detail.recipientSub, {
-        type,
-        attributes: objToKeyValueArray({
-          senderUserId: detail.senderSub,
-          senderEmail: detail.senderEmail,
-          senderFirstName: detail.senderFirstName,
-          senderLastName: detail.senderLastName,
-          senderPhoto: detail.senderPhoto,
-          amount: detail.amount,
-          note: detail.note,
-        }),
-      });
+      await sendSendMoneyRequest(type, detail);
       break;
 
     case NotificationType.AcceptChildInvitation:
-      await notificationsModel.create(detail.childSub, {
-        type,
-        attributes: objToKeyValueArray({
-          parentUserId: detail.parentSub,
-          parentFirstName: detail.parentFirstName,
-          parentLastName: detail.parentLastName,
-        }),
-      });
-      break;
-
     case NotificationType.DeclineChildInvitation:
-      await notificationsModel.create(detail.childSub, {
-        type,
-        attributes: objToKeyValueArray({
-          parentUserId: detail.parentSub,
-          parentFirstName: detail.parentFirstName,
-          parentLastName: detail.parentLastName,
-        }),
-      });
+      await sendChildInvitation(type, detail);
       break;
 
     case NotificationType.KycReview:
-      await notificationsModel.create(detail.sub, {
-        type,
-        attributes: objToKeyValueArray({
-          status: detail.status,
-        }),
-      });
+      await sendKycReview(type, detail);
       break;
 
     case NotificationType.InviteParent:
-      await notificationsModel.create(detail.childParentSub, {
-        type,
-        attributes: objToKeyValueArray({
-          childUserId: detail.childSub,
-          childEmail: detail.childEmail,
-          childFirstName: detail.childFirstName,
-          childLastName: detail.childLastName,
-          childBirthDate: detail.childBirthDate,
-          childPhoto: detail.childPhoto,
-        }),
-      });
+      await sendInviteParent(type, detail);
       break;
 
     case NotificationType.AcceptedPaidEvent:
       await acceptedPaidEventHandler(type, detail);
+      break;
+
+    default:
+      console.warn(`Unhandled notification: ${type}`);
       break;
   }
 };
---
+++ functions/events/notifications/pushNotifications.ts
@@ -0,0 +1,169 @@
+import * as OneSignal from 'onesignal-node';
+import {
+  NotificationType,
+  NotificationTeamInvitation,
+  KycStatus,
+  NotificationKycReview,
+  NotificationInviteParent,
+  NotificationChildInvitation,
+  NotificationSendMoneyRequest,
+} from './common-code/nodejs/types/notifications';
+import localeService from './localeService';
+
+const { ONESIGNAL_APP_ID = '', ONESIGNAL_API_KEY = '' } = process.env;
+const oneSignal = new OneSignal.Client(ONESIGNAL_APP_ID, ONESIGNAL_API_KEY);
+
+interface KeyAnyObject {
+  [key: string]: any;
+}
+
+interface NotificationData {
+  headings?: KeyAnyObject;
+  contents?: KeyAnyObject;
+  data?: KeyAnyObject;
+  ios_attachments?: KeyAnyObject;
+  large_icon?: string;
+  big_picture?: string;
+  ios_sound?: string;
+  android_sound?: string;
+  android_channel_id?: string;
+}
+
+class PushNotifications {
+  constructor(private imageDomain: string) {}
+
+  private getImageUrl(image: string = '') {
+    return image ? `https://${this.imageDomain}/${image}` : '';
+  }
+
+  private notification(headings: string, contents: string, imageUrl?: string) {
+    const data: NotificationData = {
+      headings: {
+        en: headings,
+      },
+      contents: {
+        en: contents,
+      },
+      ios_sound: 'default',
+      android_channel_id: '3410e243-de57-48eb-9898-c79b52b6ee3d',
+    };
+
+    if (imageUrl) {
+      data.ios_attachments = { logo: imageUrl };
+      data.large_icon = imageUrl;
+      data.big_picture = imageUrl;
+    }
+
+    return data;
+  }
+
+  private getData(language: string, type: NotificationType, detail: any): NotificationData | null {
+    const t = localeService.getFixedT(language);
+
+    switch (type) {
+      case NotificationType.AcceptTeamInvitation:
+      case NotificationType.DeclineTeamInvitation:
+        const isAccept = type === NotificationType.AcceptTeamInvitation;
+        const teamInvitation = detail as NotificationTeamInvitation;
+        const logoUrl = this.getImageUrl(teamInvitation.teamLogo);
+
+        return this.notification(
+          t('notification.teamRequest'),
+          t(
+            isAccept
+              ? 'notification.acceptedTeamInvitation'
+              : 'notification.declinedTeamInvitation',
+            {
+              teamName: teamInvitation.teamName,
+            },
+          ),
+          logoUrl,
+        );
+
+      case NotificationType.KycReview:
+        const kycReview = detail as NotificationKycReview;
+        if (kycReview.status === KycStatus.VALIDATED || kycReview.status === KycStatus.REFUSED) {
+          return this.notification(
+            t('notification.verification'),
+            t(
+              kycReview.status === KycStatus.VALIDATED
+                ? 'notification.verificationApproved'
+                : 'notification.verificationRejected',
+            ),
+          );
+        }
+
+      case NotificationType.InviteParent:
+        const inviteParent = detail as NotificationInviteParent;
+        return this.notification(
+          t('notification.familyRequest'),
+          t('notification.familyRequestMessage', {
+            name: `${inviteParent.childFirstName} ${inviteParent.childLastName}`,
+          }),
+          inviteParent.childPhoto,
+        );
+
+      case NotificationType.AcceptChildInvitation:
+      case NotificationType.DeclineChildInvitation:
+        const childAccepted = type === NotificationType.AcceptChildInvitation;
+        const childInvitation = detail as NotificationChildInvitation;
+        return this.notification(
+          t('notification.familyRequest'),
+          t(
+            childAccepted
+              ? 'notification.amilyRequestAccepted'
+              : 'notification.familyRequestRejected',
+            {
+              name: `${childInvitation.parentFirstName} ${childInvitation.parentLastName}`,
+            },
+          ),
+        );
+
+      case NotificationType.SendMoneyRequest:
+        const sendMoneyRequest = detail as NotificationSendMoneyRequest;
+        const senderName = `${sendMoneyRequest.senderFirstName} ${sendMoneyRequest.senderLastName}`;
+        const sendAmount = Number(sendMoneyRequest.amount).toLocaleString('it', {
+          style: 'currency',
+          currency: 'EUR',
+        });
+
+        return this.notification(
+          t('notification.moneyRequest'),
+          t('notification.moneyRequestMessage', {
+            name: senderName,
+            amount: sendAmount,
+            note: sendMoneyRequest.note,
+          }),
+        );
+
+      default:
+        return null;
+    }
+  }
+
+  async send(
+    language: string,
+    deviceIds: string[],
+    notificationType: NotificationType,
+    detail: any,
+  ) {
+    const data = this.getData(language, notificationType, detail);
+
+    if (!data) {
+      return;
+    }
+
+    const notification = { ...data, include_player_ids: deviceIds };
+
+    try {
+      const res = await oneSignal.createNotification(notification);
+    } catch (err) {
+      console.error({
+        err,
+        deviceIds,
+      });
+    }
+  }
+}
+
+export default PushNotifications;
