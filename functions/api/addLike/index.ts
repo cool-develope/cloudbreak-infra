149c52bd768bc31664a024cedb5bc2d9edb25f3e
cool-develope <cool199966@outlook.com>
2020-12-02 09:24:52 +0200

    Added parent approve request

--- functions/api/addLike/index.ts
+++ functions/api/addLike/index.ts
@@ -10,7 +10,27 @@ export enum FieldName {
 }
 
 const db = new AWS.DynamoDB.DocumentClient();
-const { MAIN_TABLE_NAME = '' } = process.env;
+const eventbridge = new AWS.EventBridge();
+const { MAIN_TABLE_NAME = '', IMAGES_DOMAIN = '' } = process.env;
+
+const putEvents = (type: string, detail: any) => {
+  const params = {
+    Entries: [
+      {
+        Source: 'tifo.api',
+        EventBusName: 'default',
+        Time: new Date(),
+        DetailType: type,
+        Detail: JSON.stringify(detail),
+      },
+    ],
+  };
+
+  console.log(type, detail);
+  return eventbridge.putEvents(params).promise();
+};
+
+const getImageUrl = (image: string = '') => (image ? `https://${IMAGES_DOMAIN}/${image}` : '');
 
 const getUpdateExpression = (attributes: any = {}) =>
   Object.keys(attributes)
@@ -37,7 +57,6 @@ const updateItem = (pk: string, sk: string, attributes: any) => {
     Key: { pk, sk },
     UpdateExpression: condition,
     ExpressionAttributeValues: values,
-    // ReturnValues: 'ALL_NEW',
   };
 
   return db.update(params).promise();
@@ -74,7 +93,29 @@ export const handler: Handler = async (event): Promise<{ errors: string[]; event
     await updateItem(pk, sk, { l: false });
   } else if (field === FieldName.acceptEvent) {
     if (eventMetadata.price > 0) {
-      errors.push('You need to pay for the Event');
+      const { Item: userData } = await getItem(`user#${sub}`, 'metadata');
+      const { parentUserId } = userData;
+
+      if (parentUserId) {
+        const { Item: parentApproval } = await getItem(
+          `user#${sub}`,
+          `parent-approval#event#${eventId}`,
+        );
+        if (!parentApproval) {
+          await putEvents('PendingParentApprovalForEvent', {
+            childSub: sub,
+            childFirstName: userData.firstName,
+            childLastName: userData.lastName,
+            childPhoto: getImageUrl(userData.photo),
+            parentSub: parentUserId,
+            eventId,
+            eventTitle: eventMetadata.title,
+            eventImage: getImageUrl(eventMetadata.image),
+          });
+        }
+      } else {
+        errors.push('You need to pay for the Event');
+      }
     } else {
       await updateItem(pk, sk, { a: true });
     }
