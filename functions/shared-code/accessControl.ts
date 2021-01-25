dbd49c02eaefffb9b28f48dbb32db63c3f1f9ed5
cool-develope <cool199966@outlook.com>
2021-01-25 17:00:42 +0200

    Added an ACL draft

---
+++ functions/shared-code/accessControl.ts
@@ -0,0 +1,12 @@
+export default class AccessControl {
+  /**
+   * TODO: implement 
+   * https://www.npmjs.com/package/accesscontrol
+   * https://www.npmjs.com/package/@casl/ability
+   */
+  // private checkAccess({ claims }: CognitoIdentity, clubId: string) {
+  //   const { sub, ['cognito:groups']: groups = [], ['custom:clubs']: clubs = '' } = claims;
+  //   const haveAccess = clubs.includes(clubId);
+  //   return haveAccess;
+  // }
+}
