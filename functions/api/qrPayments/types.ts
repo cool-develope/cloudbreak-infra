9362fd8476fb814b303a2401b4e0164a5186ee25
cool-develope <cool199966@outlook.com>
2021-01-25 17:01:01 +0200

    Added types

--- functions/api/qrPayments/types.ts
+++ functions/api/qrPayments/types.ts
@@ -1,10 +1,49 @@
-export interface CreateQrPaymentCategoryInput {}
-export interface UpdateQrPaymentCategoryInput {}
-export interface DeleteQrPaymentCategoryInput {}
+export interface CreateQrPaymentCategoryInput {
+  clubId: string;
+  name: string;
+  image?: string;
+  vatFee?: number;
+}
+export interface UpdateQrPaymentCategoryInput {
+  id: string;
+  clubId: string;
+  name?: string;
+  image?: string;
+}
+export interface DeleteQrPaymentCategoryInput {
+  id: string;
+  clubId: string;
+}
 
-export interface CreateQrPaymentCategoryPayload {}
-export interface UpdateQrPaymentCategoryPayload {}
-export interface DeleteQrPaymentCategoryPayload {}
+export interface CreateQrPaymentCategoryPayload {
+  errors: string[];
+  category?: QrPaymentCategory;
+}
+export interface UpdateQrPaymentCategoryPayload {
+  errors: string[];
+  category?: QrPaymentCategory;
+}
+export interface DeleteQrPaymentCategoryPayload {
+  errors: string[];
+}
+
+export interface QrPaymentCategory {
+  id: string;
+  clubId: string;
+  name: string;
+  image: string;
+  vatFee: number;
+}
+
+export interface QrPaymentCategoryDBItem {
+  pk: string;
+  sk: string;
+  name: string;
+  image: string;
+  vatFee: number;
+  createdAt: string;
+  modifiedAt: string;
+}
 
 export enum FieldName {
   createQrPaymentCategory = 'createQrPaymentCategory',
@@ -21,6 +60,26 @@ export interface FunctionEvent {
       | DeleteQrPaymentCategoryInput;
     clubId: string;
   };
-  identity: { sub: string };
+  identity: CognitoIdentity;
   info: { fieldName: FieldName };
+}
+
+export interface CognitoIdentity {
+  sub: string;
+  claims: CognitoClaims;
+}
+
+export interface CognitoClaims {
+  sub: string;
+  aud: string;
+  token_use: string;
+  email: string;
+  'cognito:groups': string[];
+  'cognito:username': string;
+  'custom:trzUserId': string;
+  'custom:clubs': string;
+  'custom:trzWalletsId': string;
+  'custom:trzScopes': string;
+  'custom:trzCardsId': string;
+  'custom:trzChildren': string;
 }
